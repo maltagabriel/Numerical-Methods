c9d017a5c889e4e42fc85619165ab3902e77425e
Gabriel Malta <64297513+maltagabriel@users.noreply.github.com>
2021-07-23 19:40:40 -0300

    Delete Numerical-Methods directory

--- Numerical-Methods/Método de Newton/fl.m
+++
@@ -1,5 +0,0 @@
-% Também deixei a derivada definida explcitamente, uma ideia seria a partir da f definida, calcular sua derivada numericamente e aplicar o método (?)
-
-function yl = fl(x)
-  yl=-(x-1);
-endfunction
